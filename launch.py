c9b111a79f182f96dc14d0a3d17cff2de07e3c9a
Sridhar Matta <mattasridhar19@gmail.com>
2020-09-06 18:02:39 -0400

    GifShwnInUI&SaveCancelAction

--- launch.py
+++ launch.py
@@ -19,22 +19,22 @@ canvas = tkt.Canvas(window, width=700, height=350,
 canvas.pack()
 window.resizable(False, False)  # to prevent user from resizing the window
 window.title(titleText)  # window title
-frame = tkt.Frame(window, bg='#b5b5b5')  # frame creation
-frame.place(relwidth=0.8,
-            relheight=0.8, relx=0.1, rely=0.1)
 
 gifFrmImgBtn = tkt.Button(window, text=gifFrmImgBtnText, padx=0.5,
-                          pady=0.5, bg='#454545', borderwidth=0, command=lambda: gifEngineer.loadImgSrcDir(frame)).place(x=0, y=0)  # creating button and adding to frame
+                          pady=0.5, bg='#454545', borderwidth=0, command=lambda: gifEngineer.loadImgSrcDir(window)).place(x=0, y=0)  # creating button and adding to frame
 # gifFrmImgBtn.pack(side='left')
 
 gifFrmVidBtn = tkt.Button(window, text=gifFrmVidBtnText, padx=0.5,
-                          pady=0.5, bg='#454545', borderwidth=0, command=lambda: gifEngineer.loadImgSrcDir(frame)).place(x=532, y=0)  # creating button and adding to frame
+                          pady=0.5, bg='#454545', borderwidth=0, command=lambda: gifEngineer.loadImgSrcDir(window)).place(x=532, y=0)  # creating button and adding to frame
 # gifFrmVidBtn.pack(side='right')
 
 # dirBtn = tkt.Button(frame, text=imageDirBtnText, padx=0.5,
 #                     pady=0.5, bg='#b5b5b5', borderwidth=0, command=lambda: gifEngineer.loadImgSrcDir(frame))  # creating button and adding to frame
 # dirBtn.pack()
 # dirBtn.place(bordermode='outside', relwidth=0.5, relheight=0.5, relx=0.1, rely=0.1)
+
+# handle window close
+window.protocol("WM_DELETE_WINDOW", gifEngineer.windowClose(window))
 
 # show the created window
 window.mainloop()
